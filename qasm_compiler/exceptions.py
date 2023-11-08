294b6f25e09cbdb7735a9d9ebeed0ced113bda92
chensgit169 <weichen191@mails.ucas.ac.cn>
2023-11-08 15:45:40 +0800

    delete qasm-compiler

--- qasm_compiler/exceptions.py
+++
@@ -1,2 +0,0 @@
-class MissingOptionalLibraryError(Exception):
-    pass
--- qasm_compiler/test/test.py
+++
@@ -1,9 +0,0 @@
-from compiler.qasm.qasm import Qasm
-
-file_path = 'examples/simple.qasm'
-
-with open(file_path, 'r') as qfile:
-    data = qfile.read()
-
-qasm = Qasm(data=data)
-print(type(qasm.parse()))
