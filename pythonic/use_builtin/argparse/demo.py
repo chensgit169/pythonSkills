43c951b9a3034a59e5a9ce3b43daea55696cc75d
chen_wei <weichen191@mails.ucas.ac.cn>
2024-03-26 10:11:55 +0800

    build: add demo of CMD argparse

---
+++ pythonic/use_builtin/argparse/demo.py
@@ -0,0 +1,31 @@
+import argparse
+import os
+
+
+"""
+Usage: python demo.py <operation> <args> in CMD
+"""
+
+# Create an argument parser
+parser = argparse.ArgumentParser(description="CMD operations example")
+
+# Add positional arguments
+parser.add_argument("operation", choices=["echo", "mkdir", "rmdir"], help="Operation to perform")
+parser.add_argument("args", nargs="*", help="Operation arguments")
+
+# Parse the command-line arguments
+args = parser.parse_args()
+
+# Perform the corresponding operation
+if args.operation == "echo":
+    # Construct the command for 'echo' operation
+    command = "echo " + " ".join(args.args)
+elif args.operation == "mkdir":
+    # Construct the command for 'mkdir' operation
+    command = "mkdir " + " ".join(args.args)
+elif args.operation == "rmdir":
+    # Construct the command for 'rmdir' operation
+    command = "rmdir " + " ".join(args.args)
+
+# Execute the command in CMD
+os.system(command)
